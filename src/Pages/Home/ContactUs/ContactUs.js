1d9eecddccac0320fc1308af6a0a4c1d8eac3cfb
shamim <shamimhossen3912@gmail.com>
2023-03-15 13:37:18 +0600

    added contact-us section

---
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./ContactUs.css";
+
+const ContactUs = () => {
+  return (
+    <div className="contact-container h-screen w-full flex items-center">
+      <div className="text-center w-full">
+        <h4 className="text-primary font-bold text-lg">Testimonial</h4>
+        <h1 className="text-4xl mx-auto text-white">What Our Patients Says</h1>
+        <div className="mt-12">
+          <form className="flex flex-col items-center justify-center">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered input-info w-[450px]"
+            />
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered input-info  w-[450px] mt-4"
+            />
+            <textarea
+              className="textarea textarea-info w-[450px] h-[136px] mt-4"
+              placeholder="Bio"
+            ></textarea>
+            <input
+              type="submit"
+              className="mt-4 btn btn-primary bg-gradient-to-r from-primary to-secondary text-white mt-[155px]"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AppoinmentBanner from "../AppoinmentBanner/AppoinmentBanner";
 import Banner from "../Banner/Banner";
+import ContactUs from "../ContactUs/ContactUs";
 import InfoCards from "../InfoCards/InfoCards";
 import Services from "../Services/Services";
 import Testimonials from "../Testimonials/Testimonials";
@@ -13,6 +14,7 @@ const Home = () => {
       <Services></Services>
       <AppoinmentBanner></AppoinmentBanner>
       <Testimonials></Testimonials>
+      <ContactUs></ContactUs>
     </div>
   );
 };
